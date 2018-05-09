84a52add1a4ddefa28d93fe6fa3db801bdaecd97
msychova <msychova@exadel.com>
2018-05-09 19:48:25 +0300

    ToDoPaper L4

---
+++ ToDoPaper/dom.js
@@ -0,0 +1,69 @@
+let ListDiv = document.getElementById("todo-items");
+
+function DOM(){
+	this.addTodoItemDom = function(todoItem){
+		
+		let param;
+
+		if(todoItem.completed === false)
+		param = `<div>${JSON.stringify(todoItem.id)}.${JSON.stringify(todoItem.text)} <input type="checkbox" name="ToDoItem" value="not_completed"></div>`; 
+		
+		if(todoItem.completed === true)
+		param = `<div>${JSON.stringify(todoItem.id)}.${JSON.stringify(todoItem.text)} <input type="checkbox" name="ToDoItem" value="completed" checked></div>`; 
+		
+		ListDiv.innerHTML += param;
+		// document.body.innerHTML = ListDiv;
+		
+		console.log(ListDiv);
+	}
+	this.viewTodoListDom = function(todoItem){
+		
+		let param = "";
+		console.log(todoItem);
+		
+		for(let i=0;i < todoItem.length;i++){
+			if(todoItem[i].completed === false)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="not_completed"></div>`); 
+
+			if(todoItem[i].completed === true)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="completed" checked></div>`); 
+		}
+		
+		console.log(param);
+		ListDiv.innerHTML = param;
+	}
+	
+	this.editTodoItemDom = function(todoItem){
+		let param = "";
+		
+		for(let i=0;i < todoItem.length;i++){
+			if(todoItem[i].completed === false)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="not_completed"></div>`); 
+
+			if(todoItem[i].completed === true)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="completed" checked></div>`); 
+		}
+		
+		console.log(param);
+		ListDiv.innerHTML = param;
+		
+
+	}
+	this.deleteTodoItemDom = function(todoItem){
+		let param = "";
+		
+		for(let i=0;i < todoItem.length;i++){
+			if(todoItem[i].completed === false)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="not_completed"></div>`); 
+
+			if(todoItem[i].completed === true)
+			param += (`<div>${JSON.stringify(todoItem[i].id)}.${JSON.stringify(todoItem[i].text)} <input type="checkbox" name="ToDoItem" value="completed" checked></div>`); 
+		}
+		
+		console.log(param);
+		ListDiv.innerHTML = param;
+
+	}
+}
+
+var domFunction = new DOM();
--- ToDoPaper/functions.js
+++ ToDoPaper/functions.js
@@ -1,12 +1,6 @@
-/*let todoItem = {
-	text: 'Default',
-	completed: false,
-	id: 0
-};*/
-
-let itemsType = 'not_completed';
-let todoItemId = 0;
-let newText = 'text';
+let todoArr = []; //array with all todo items
+let tdid = 0; //default value of todo id
+let newItem = {};
 
 function ToDoItem(todoItem){
 	
@@ -21,74 +15,126 @@ function ToDoItem(todoItem){
 	
 	initToDoHistory();
 	
-	function todoItem (text,completed){
-		text = 'Default';
-		completed = false;
-		id = 0;
+	function todoItem (text,completed,id){
+		this.text = text;
+		this.completed = completed;
+		this.id = tdid ++;
 	}
 
+//---create new todo item
+	
 	this.addTodoItem = function(text,completed) {
-		if (text != null && completed != null){
-			let newItem = new todoItem(text,completed);
-								
-		}
+		let newItem = {};
+		newItem = new todoItem(text,completed);
 		
+		if (!text || completed == null || text === false){
+			
+			return `Text and completed status are required.`;								
+		}	
+		
+		domFunction.addTodoItemDom(newItem);
+		todoArr.push(newItem);
+
 		ToDoHistory.push({
 			date: (new Date()).toISOString(),
 			text: newItem.text,
-			completed: newItem.completed
+			completed: newItem.completed,
+			id: newItem.id
 		});
 
-		console.log(ToDoHistory);
+		console.log('todoArr:' + JSON.stringify(todoArr));
 		
-		return newItem;	
-
-
+		return (this.todoArr);	
 	};
 
+//---view list of todo items
+	
 	this.viewTodoList = function(itemsType) {
-	if (itemsType == 'completed'){
+	
+		let temp = [];
 		
-		todoItem.filter = (type) => todoItem.completed == true;
-		return todoItem;
-	}
-	if (itemsType == 'not_completed'){
-		
-		todoItem.filter = (type) => todoItem.completed != true;
-		return todoItem;
-		
-	}
-	if (itemsType == 'all'){
-		return todoItem;
-	}
-};
+		if (itemsType === 'completed'){				
+			temp = todoArr.filter(todoItem => todoItem.completed === true); 
+			console.log(temp);
+			domFunction.viewTodoListDom(temp);	
 
-function editTodoItem(todoItemId, newText) {
-	if(newText){
-		if (todoItem.id === todoItemId){
-			todoItem.text = newText;
-			return true;
+		}
+
+		if (itemsType == 'not_completed'){			
+			temp = todoArr.filter(todoItem => todoItem.completed === false); 
+			console.log(temp);
+			domFunction.viewTodoListDom(temp);
 		}
 		
-		ToDoHistory.push({
-			date: (new Date()).toISOString(),
-			text: todoItem.text,
-			completed: todoItem.completed
-		});
+		if (itemsType == 'all'){
+			domFunction.viewTodoListDom(todoArr);
+			return todoArr;
+		}
+};
+
+//---edit todo item text by id---
+ 	this.editTodoItem = function (todoItemId,newText) {
+		let flag = false;
+		let msg = '';
+		
+		if (!newText || todoItemId === false){
+			
+			return `Please, add text.`;								
+		}
+		
+		for(let i=0; i < todoArr.length; i++){
+			if (todoArr[i].id === todoItemId){
+				flag = true;
+				todoArr[i].text = newText;
+				console.log(JSON.stringify(todoArr));
+				domFunction.editTodoItemDom(todoArr);
+			}
+		} 
+		
+		flag === false ? msg = `Item with ID: ${todoItemId} doesn\'t exist.`: msg = `Item with ID: ${todoItemId} was updated.`;
+		
+		return msg;	
 	}
 	
-}
-function deleteTodoItem(todoItemId) {
-	if (todoItem.id === todoItemId){
-		delete todoItem;
-		return true;
+//--- delete todo item---
+	
+	this.deleteTodoItem = function (todoItemId) {
+		let flag = false;
+		let msg = '';
+		
+		for(let i=0; i < todoArr.length; i++){
+			if (todoArr[i].id === todoItemId){
+				flag = true;
+				todoArr.splice(i,1);
+				console.log(JSON.stringify(todoArr));								
+				domFunction.deleteTodoItemDom(todoArr);
+			}
+		} 
+		
+		flag === false ? msg = `Item with ID: ${todoItemId} doesn\'t exist.`: msg = `Item with ID: ${todoItemId} was deleted.`;
+		
+		return msg;	
 	}
-}
-function completeTodoItem(todoItemId) {
-	if (todoItem.id === todoItemId){
-		todoItem.completed = true;
+	
+//--- complete todo item
+ 	this.completeTodoItem = function (todoItemId) {
+		let flag = false;
+		let msg = '';
+		
+		for(let i=0; i < todoArr.length; i++){
+			if (todoArr[i].id === todoItemId){
+				flag = true;
+				todoArr[i].completed = true;
+				console.log(JSON.stringify(todoArr));								
+			}
+		} 
+		
+		flag === false ? msg = `Item with ID: ${todoItemId} doesn\'t exist.`: msg = `Status of Item with ID: ${todoItemId} was updated to completed.`;
+		
+		return msg;	
 	}
-}
 }
 
 var todoitem = new ToDoItem();
+
+
